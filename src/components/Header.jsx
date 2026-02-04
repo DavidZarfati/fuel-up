a444d5eba5d4d7c791a045642c1dce86cfb3a220
SwitchTia <oblasser.tatiana@gmail.com>
2026-02-04 12:17:57 +0100

    Feat: Header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
+import "./Header.css";
+import logo from "../assets/images/logo.jpg";
+import { useState } from "react";
+import axios from "axios";
+
 
 
 
@@ -13,14 +18,60 @@ export default function Header({ nameApp }) {
     ];
 
 
+
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const backendBaseUrl = import.meta.env.VITE_BACKEND_URL;
+
+
+    function searchProduct() {
+
+        if (!search.trim()) {
+            return;
+        }
+
+        axios
+            .get(`${backendBaseUrl}/api/products/search?key=${search}`)
+            .then((resp) => {
+
+                const products = resp.data.results || [];
+
+                if (products.length > 0) {
+                    const firstProduct = products[0];
+
+                    navigate(`/productss/${firstProduct.slug}`);
+
+                    setSearch("");
+                } else {
+
+                    alert("Non abiamo trovato nessun prodotto con questo nome");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+
+            });
+    }
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        searchProduct();
+    }
+
+
     return (
         <header className="header">
             <div className="header-content">
-                <div className="logo">FuelUp</div>
-                <div>
+
+                <div className="flex-inline">
+                    <img className="logo" src={logo} alt="" />
+
+
                     <ul className="nav-item">
                         {headerLinks.map((link, index) => (
-                            <li  key={index}>
+                            <li key={index}>
                                 <NavLink
                                     className="nav-link"
                                     aria-current="page"
@@ -31,6 +82,22 @@ export default function Header({ nameApp }) {
                             </li>
                         ))}
                     </ul>
+                </div>
+
+
+
+                <div>
+                    <form className="search-bar" onSubmit={handleSubmit}>
+                        <input
+                            type="search"
+                            placeholder="Cosa cerchi?"
+                            value={search}
+                            onChange={(event) => setSearch(event.target.value)}
+                        />
+
+
+                        <button type="submit">Cerca</button>
+                    </form>
                 </div>
 
             </div>
